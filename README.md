d9a27b54b90ce36e5ed88152b5c571f5485b88f5
Glitch (hello-express) <none>
2019-05-13 15:52:53 +0000

    🚇⛱ Checkpoint ./README.md:3234086/798 ./quotes.json:3234086/13985 ./server.js:3234086/3833

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 A simple node express app running on Glitch
 =================
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+You can change all of this README.md - make it yours.
 
+Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
 
 
 Your Project
 ------------
 
 - your app starts at `server.js`
-- add frameworks and packages in `package.json`
+- you can add frameworks and packages in `package.json`
 
 
 About Glitch
--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 // server.js
-// where your node app starts
+// This is where your node app starts
 
-// init project
-const express = require('express');
-
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
 const app = express();
 
-app.get('/', function(request, response) {
-  response.send("hello CYF")
+//load the quotes JSON
+const Quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function(request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
+//START OF YOUR CODE...
 
-app.get('/data', function(request, response) {
-  response.json([1,2,3])
-});
 
-// listen for requests :)
+
+
+
+
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
-
-
-//read about routing
-// http://expressjs.com/en/starter/basic-routing.html
